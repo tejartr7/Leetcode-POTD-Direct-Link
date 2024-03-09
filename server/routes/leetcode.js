3b393b629fd060313c1d5f150086659cd135448b
tejartr7 <raghusai126@gmail.com>
2024-03-09 18:38:20 +0530

    tejartr commits

--- server/routes/leetcode.js
+++ server/routes/leetcode.js
@@ -1,31 +1,32 @@
 import { LeetCode } from "leetcode-query";
 import express from "express";
 import cors from "cors";
+
 const leetcode = new LeetCode();
 const router = express.Router();
-router.use(
-  cors({
-    origin: "*",
-  })
-);
+
+router.use(cors({
+  origin: "*",
+}));
+
 router.use(express.json());
+
 router.get("/dailyChallenge", async (req, res) => {
   console.log("leetcode daily challenge called");
   try {
     const daily = await leetcode.daily();
     if (daily && daily.link) {
       console.log("Redirecting to: ", "https://leetcode.com" + daily.link);
-      res.status(200).json(daily.link);
-      //res.send(daily.link + " " + daily.title);
+      res.redirect("https://leetcode.com" + daily.link);
     } else {
       res.status(404).json({ message: "Daily challenge link not found" });
-      //res.send("Daily challenge link not found");
     }
   } catch (error) {
     console.error("Error fetching daily challenge:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
 router.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   // Add other CORS headers if needed
@@ -35,4 +36,5 @@ router.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
+
 export default router;

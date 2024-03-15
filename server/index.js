f5c87035e87a74de727b47937b79d08405a634f2
tejartr7 <raghusai126@gmail.com>
2024-03-15 16:16:51 +0530

    fixing backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 import express from "express";
 import cors from "cors";
-import fetch from "node-fetch"; // Import fetch to make HTTP requests
-import { LeetCode, fetcher } from "leetcode-query";
-import { chromium } from "playwright-extra";
-import stealth from "puppeteer-extra-plugin-stealth";
+import { LeetCode } from "leetcode-query";
 
-const BASE_URL = "https://leetcode.com/graphql";
 const app = express();
 
 app.use(express.json());
@@ -15,44 +11,27 @@ app.use(
   })
 );
 
-fetcher.set(async (...args) => {
-  const browser = await chromium.use(stealth()).launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto(args[0].toString());
-
-  const res = await page.evaluate(async (args) => {
-    const res = await fetch(...args);
-    return {
-      body: await res.text(),
-      status: res.status,
-      statusText: res.statusText,
-      headers: Object.fromEntries(res.headers.entries()),
-    };
-  }, args);
-
-  await browser.close();
-  return new fetch.Response(res.body, res); // Fix: Use fetch.Response
-});
-
 async function main() {
   console.log("Fetching daily question");
   const lc = new LeetCode();
   var daily = "";
   try{
-    daily=await lc.daily();
+    daily = await lc.daily();
   }
-  catch(e){
+  catch (e) {
     console.log(error);
   }
   return daily;
 }
 
+const _cache = main();
+
+app.get("/", async (req, res) => {
+  const daily = await _cache;
+  res.redirect('https://leetcode.com' + daily.link)
+});
+
 const PORT = 8000;
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log("server started on http://localhost:8000");
-  const daily = await main();
-  app.get("/", (req, res) => {
-    res.redirect('https://leetcode.com' + daily.link)
-  });
 });
